8643b5cbe75d00416d7741aeb52c3e98a85866a1
byelipk <byelipk@gmail.com>
2016-08-29 14:01:23 -0400

    Add custom if-else macro

--- lib/control_flow.ex
+++ lib/control_flow.ex
@@ -11,4 +11,14 @@ defmodule ControlFlow do
     end
   end
 
+  defmacro my_if(expr, do: if_block), do: if(expr, do: if_block, else: nil)
+  defmacro my_if(expr, do: if_block, else: else_block) do
+    quote do
+      case unquote(expr) do
+        result when result in [false, nil] -> unquote(else_block)
+                                   _truthy -> unquote(if_block)
+      end
+    end
+  end
+
 end
--- test/control_flow_test.exs
+++ test/control_flow_test.exs
@@ -2,14 +2,29 @@ defmodule ControlFlowTest do
   use ExUnit.Case
   doctest ControlFlow
 
-  test "unless keyword runs block when test condition fails" do
+  test "unless runs block when test condition fails" do
     result = ControlFlow.unless(1 == 2, do: "block entered")
     assert result == "block entered"
   end
 
-  test "unless keyword skips block when test condition passes" do
+  test "unless skips block when test condition passes" do
     result = ControlFlow.unless(1 == 1, do: "block entered")
     assert result != "block entered"
+  end
+
+  test "my_if runs if_block when test condition is truthy" do
+    result = ControlFlow.my_if(true, do: "block entered")
+    assert result == "block entered"
+  end
+
+  test "my_if runs else_block when test condition is falsy" do
+    result = ControlFlow.my_if(false, do: true, else: "block entered")
+    assert result == "block entered"
+  end
+
+  test "my_if returns default value when else_block is not provided" do
+    result = ControlFlow.my_if(false, do: true)
+    assert result == nil
   end
 
 end
