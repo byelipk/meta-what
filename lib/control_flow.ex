efe410076b3383c4abe771c114dc5cbb8dd58f99
byelipk <byelipk@gmail.com>
2016-08-29 13:45:30 -0400

    Rewrite ControlFlow.unless using a case statement

--- lib/control_flow.ex
+++ lib/control_flow.ex
@@ -2,7 +2,12 @@ defmodule ControlFlow do
 
   defmacro unless(expression, do: block) do
     quote do
-      if !unquote(expression), do: unquote(block)
+      # if !unquote(expression), do: unquote(block)
+      case unquote(expression) do
+        false    -> unquote(block)
+        nil      -> unquote(block)
+        _truthy  -> false
+      end
     end
   end
 
